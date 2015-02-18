b138ec4f02e06158c79b82f8e946a8e9e35ba6af
Eduardo Ochoa <eduardo8af1@gmail.com>
2015-02-17 21:06:04 -0600

    Implemente un par de metodos a user, termino de tuto 2

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,34 @@
  */
 
 module.exports = {
+	findUserbyEmail:function(req,res)
+  {
+    var id = req.param('id');
+    User.findOne({email:id})
+        .exec(function(err,user){
+
+          if(err)
+            res.json({error:err});
+          if(user === undefined)
+            res.json({notFound:true});
+          else
+            res.json({notFound:false,userData:user});
+        });
+  },
 	
+	findUserbyNumber:function(req,res)
+  {
+    var id = req.param('id');
+    User.findOne({empnum:id})
+        .exec(function(err,user){
+
+          if(err)
+            res.json({error:err});
+          if(user === undefined)
+            res.json({notFound:true});
+          else
+            res.json({notFound:false,userData:user});
+        });
+  }
 };
 
